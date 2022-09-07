c4d6aa5a4f7021a8e88fd4ab501bc5ce4b264114
Nadeen ElGendi <93093946+NadeenElGendi@users.noreply.github.com>
2022-09-07 18:40:09 +0200

    Update productDetailScreen.dart

--- lib/ui/productDetailScreen.dart
+++ lib/ui/productDetailScreen.dart
@@ -1,17 +1,102 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/rendering.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dots_indicator/dots_indicator.dart';
+import 'const/appcolors.dart';
 class ProductDetailScreen extends StatefulWidget {
   var _product;
   ProductDetailScreen(this._product);
   @override
   _ProductDetailScreenState createState() => _ProductDetailScreenState();
 }
-
 class _ProductDetailScreenState extends State<ProductDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            radius: 15,
+            backgroundColor: AppColors.deep_orange,
+            child: IconButton(
+                onPressed: () => Navigator.pop(context),
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                )),
+          ),
+        ),
+        actions: [
+          CircleAvatar(
+            radius: 15,
+            backgroundColor: AppColors.deep_orange,
+            child: IconButton(
+                onPressed: () => Navigator.pop(context),
+                icon: Icon(
+                  Icons.favorite_outline,
+                  color: Colors.white,
+                )),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 12, right: 12, top: 10),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AspectRatio(
+                aspectRatio: 3.5,
+                child: CarouselSlider(
+                    items: widget._product['proudct-img']
+                        .map<Widget>((item) => Padding(
+                              padding: const EdgeInsets.only(left: 3, right: 3),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: NetworkImage(item),
+                                        fit: BoxFit.fitWidth)),
+                              ),
+                            ))
+                        .toList(),
+                    options: CarouselOptions(
+                        autoPlay: false,
+                        enlargeCenterPage: true,
+                        viewportFraction: 0.8,
+                        enlargeStrategy: CenterPageEnlargeStrategy.height,
+                        onPageChanged: (val, carouselPageChangedReason) {
+                          setState(() {
+                            _dotPosition = val;
+                          });
+                        })),
+              ),
+              Text(widget._product['product-name']),
+              Text(widget._product['product-description']),
+              Text(widget._product['product-price'].toString()),
+              SizedBox(
+                width: 1.sw,
+                height: 56.h,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "ADD TO CART",
+                    style: TextStyle(color: Colors.white, fontSize: 18.sp),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      primary: AppColors.deep_orange, elevation: 3),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
