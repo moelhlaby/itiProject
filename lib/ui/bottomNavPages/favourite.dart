6d3b0c59792a85ec5ace34f5afa32d418b6c433f
Tasniem Seiam <tasniemseiam@gmail.com>
2022-09-08 01:21:41 +0200

    Update favourite.dart

--- lib/ui/bottomNavPages/favourite.dart
+++ lib/ui/bottomNavPages/favourite.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:iti_project/widgets/fetchProducts.dart';
+import '../../widgets/fetchProducts.dart';
 
 class Favourite extends StatefulWidget {
   @override
