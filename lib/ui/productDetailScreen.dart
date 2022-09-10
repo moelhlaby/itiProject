20dd9e0f480cf2824b1c227683556fd5f73b8a42
Nadeen ElGendi <93093946+NadeenElGendi@users.noreply.github.com>
2022-09-10 13:59:30 +0200

    Update productDetailScreen.dart
    update ui

--- lib/ui/productDetailScreen.dart
+++ lib/ui/productDetailScreen.dart
@@ -1,4 +1,3 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,7 +8,7 @@ import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import '../const/AppColors.dart';
+import 'package:iti_project/const/appColors.dart';
 
 class ProductDetailScreen extends StatefulWidget {
   var _product;
@@ -23,7 +22,7 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
     final FirebaseAuth _auth = FirebaseAuth.instance;
     var currentUser = _auth.currentUser;
     CollectionReference _collectionRef =
-    FirebaseFirestore.instance.collection("users-cart-items");
+        FirebaseFirestore.instance.collection("users-cart-items");
     return _collectionRef
         .doc(currentUser!.email)
         .collection("items")
@@ -39,7 +38,7 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
     final FirebaseAuth _auth = FirebaseAuth.instance;
     var currentUser = _auth.currentUser;
     CollectionReference _collectionRef =
-    FirebaseFirestore.instance.collection("users-favourite-items");
+        FirebaseFirestore.instance.collection("users-favourite-items");
     return _collectionRef
         .doc(currentUser!.email)
         .collection("items")
@@ -60,7 +59,7 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
         leading: Padding(
           padding: const EdgeInsets.all(8.0),
           child: CircleAvatar(
-            radius: 15,
+            radius: 25,
             backgroundColor: AppColors.deep_orange,
             child: IconButton(
                 onPressed: () => Navigator.pop(context),
@@ -85,22 +84,21 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
               return Padding(
                 padding: const EdgeInsets.only(right: 8.0),
                 child: CircleAvatar(
-                  radius: 15,
+                  radius: 20,
                   backgroundColor: AppColors.deep_orange,
                   child: IconButton(
                     onPressed: () => snapshot.data.docs.length == 0
                         ? addToFavourite()
-                        : Fluttertoast.showToast(
-                        msg: "already added"),
+                        : Fluttertoast.showToast(msg: "already added"),
                     icon: snapshot.data.docs.length == 0
                         ? Icon(
-                      Icons.favorite_outline,
-                      color: Colors.white,
-                    )
+                            Icons.favorite_outline,
+                            color: Colors.white,
+                          )
                         : Icon(
-                      Icons.favorite,
-                      color: Colors.white,
-                    ),
+                            Icons.favorite,
+                            color: Colors.white,
+                          ),
                   ),
                 ),
               );
@@ -110,51 +108,77 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.only(left: 12, right: 12, top: 10),
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AspectRatio(
-                aspectRatio: 3.5,
-                child: CarouselSlider(
-                    items: widget._product["product-img"]
-                        .map<Widget>((item) => Padding(
-                      padding: const EdgeInsets.only(left: 3, right: 3),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: NetworkImage(item),
-                                fit: BoxFit.fitWidth)),
-                      ),
-                    ))
-                        .toList(),
-                    options: CarouselOptions(
-                        autoPlay: false,
-                        enlargeCenterPage: true,
-                        viewportFraction: 0.8,
-                        enlargeStrategy: CenterPageEnlargeStrategy.height,
-                        onPageChanged: (val, carouselPageChangedReason) {
-                          setState(() {
-                          });
-                        })),
-              ),
-              Text(widget._product["product-name"]),
-              Text(widget._product["product-description"]),
-              Text(widget._product["product-price"].toString()),
-              SizedBox(
-                width: 1.sw,
-                height: 56.h,
-                child: ElevatedButton(
-                  onPressed: () => addToCart(),
-                  child: Text(
-                    "ADD TO CART",
-                    style: TextStyle(color: Colors.white, fontSize: 18.sp),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                      primary: AppColors.deep_orange, elevation: 3),
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AspectRatio(
+                  aspectRatio: 3.5,
+                  child: CarouselSlider(
+                      items: widget._product["product-img"]
+                          .map<Widget>((item) => Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 3, right: 3),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: NetworkImage(item),
+                                          fit: BoxFit.fitWidth)),
+                                ),
+                              ))
+                          .toList(),
+                      options: CarouselOptions(
+                          autoPlay: false,
+                          enlargeCenterPage: true,
+                          viewportFraction: 0.8,
+                          enlargeStrategy: CenterPageEnlargeStrategy.height,
+                          onPageChanged: (val, carouselPageChangedReason) {
+                            setState(() {});
+                          })),
                 ),
-              )
-            ],
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  widget._product["product-name"],
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 27),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(widget._product["product-description"],
+                    style: TextStyle(fontSize: 20, color: Colors.black)),
+                SizedBox(height: 25),
+                Text(widget._product["product-price"].toString() + "" + "\$",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: AppColors.deep_orange)),
+                SizedBox(
+                  height: 70,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: SizedBox(
+                    width: 1.sw,
+                    height: 56.h,
+                    child: ElevatedButton(
+                      onPressed: () => addToCart(),
+                      child: Text(
+                        "ADD TO CART",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          primary: AppColors.deep_orange, elevation: 3),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
